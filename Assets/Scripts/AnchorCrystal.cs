2cff6126ccf493b1acd808bda47ee535d432d626
Unknown <rmh472@nyu.edu>
2019-10-22 16:22:48 -0400

    Crystal movement changed from translate to physics

---
+++ Assets/Scripts/AnchorCrystal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorCrystal : MonoBehaviour
+{
+    private GameObject gameManager;
+    private Game_Manager manager;
+
+    public GameObject target;
+    Rigidbody rb;
+    CapsuleCollider cc;
+    Vector3 moveDir;
+    public float moveSpeed;
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
+        manager = gameManager.GetComponent<Game_Manager>();
+        rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (manager.aiming)
+        {
+            cc.isTrigger = false;
+        }
+        else
+        {
+            cc.isTrigger = true;
+        }
+        moveDir = new Vector3(transform.position.x - target.transform.position.x, transform.position.y - target.transform.position.y, transform.position.z - target.transform.position.z);
+        //moveDir.Normalize();
+        rb.velocity = -moveDir * Time.deltaTime * moveSpeed;
+    }
+}
