0b56f0b55ea0f252fb8cc30019e6011b2f46a6c4
Unknown <rmh472@nyu.edu>
2019-10-24 17:06:43 -0400

    Shitty Crystal Count UI Element

---
+++ Assets/Scripts/UIRotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRotate : MonoBehaviour
+{
+    private GameObject gameManager;
+    private Game_Manager manager;
+
+    RectTransform rt;
+    public Text count;
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
+        manager = gameManager.GetComponent<Game_Manager>();
+
+        rt = GetComponent<RectTransform>();
+        zRot = 0;
+    }
+
+    float zRot;
+    void Update()
+    {
+        count.text = manager.playerStones.Count + "";
+        zRot+=.1f;
+        rt.rotation = Quaternion.Euler(0, 0, zRot);
+    }
+}
