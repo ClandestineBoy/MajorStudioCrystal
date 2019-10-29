bc19ebb9291b34e1e96838b1127d999465045ad9
Unknown <rmh472@nyu.edu>
2019-10-29 16:28:43 -0400

    Partial Crystal Lock Door Stuff

--- Assets/Scripts/CrystalLock.cs
+++ Assets/Scripts/CrystalLock.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CrystalLock : MonoBehaviour
 {
+    private GameObject gameManager;
+    private Game_Manager manager;
+
+    public GameObject door;
+    public List<GameObject> stonesTaken;
+    public int crystalRequired;
+    public Text display;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
+        manager = gameManager.GetComponent<Game_Manager>();
+
+        display.text = crystalRequired + "";
+        door = transform.parent.gameObject;
+        Debug.Log(transform.parent.gameObject.name);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(manager.playerStones.Count >= crystalRequired)
+            door.SetActive(false);
+
+        }
     }
 }
